f67d192629a0fc0e984da57c5773bf34ccac55ea
baiyang <yang01.bai@hobot.cc>
2018-11-20 09:26:53 +0800

    Add test net

--- README.md
+++ README.md
@@ -6,9 +6,16 @@ python学习代码
 
 testfunc      #学习函数相关
 testunittest  #学习unittest
+testnet       #学习socket相关
 testmulthread #学习多线程
 testprocess   #学习多进程
 testIterater  #学习迭代器
 testgenerator #学习生成器
 testyield     #学习协程
 testregular   #学习正则表达式
+
+
+
+python学习要点：
+1.完成项目时，先尝试用最简单的代码完成代码的流程。不要想的一次完美。
+2.然后慢慢修改即可。
---
+++ testnet/testnonblock/testserver1.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   testserver1.py
+@Time    :   2018/11/20 09:07:20
+@Author  :   BaiYang 
+@Version :   1.0
+@Contact :   yang01.bai@horizon.ai
+@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+@Desc    :   测试tcp的非阻塞模式1：使用非阻塞单任务为多个客户端服务。
+'''
+
+import random,os,time,sys
+import socket
+
+
+def main():
+    client_list = list()
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # availd address reused
+    server.bind(("",9998))
+    server.listen(128)
+
+    server.setblocking(False)
+    while True:
+        time.sleep(0.5)  #debug
+        try:
+            new_sock,_ = server.accept()
+        except Exception as e:      # no client connect
+            print("没有客户端链接......")
+            pass
+        else:
+            print("有一个客户端链接(%s)......" % str(new_sock))
+            new_sock.setblocking(False)
+            client_list.append(new_sock)
+            
+        for client in client_list:
+            try:
+                content = client.recv(0x1024)
+            except Exception as e:  # not read data
+                print("---没有数据---")
+                pass
+            else:
+                if content:
+                    print(content.decode("utf-8")) # 处理数据
+                else:    # 客户端关闭
+                    print("客户端(%s)关闭<<<" % str(client))
+                    client.close()
+                    client_list.remove(client)
+
+
+if __name__ == "__main__":
+    main()
